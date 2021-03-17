95aa1312d053f7a2e0a723069ebd41c621d52803
Rafael Thomas <contato.rafaelthomas@gmail.com>
2021-03-17 18:23:58 -0300

    new

---
+++ platforms/browser/index.mjs
@@ -0,0 +1,30 @@
+export default class TableWebComponent {
+    render(data) {
+        const htmlTemplate = this.prepareData(data)
+
+        document.body.insertAdjacentHTML('afterBegin', htmlTemplate)
+    }
+    prepareData(data) {
+        const  [firstItem]  = data
+        const joinLists = list => list.join('')
+        const tHeaders = Object.keys(firstItem)
+            .map(text => `<th scope=col>${text}</th>`)
+
+        const tBody = data
+            .map(item => Object.values(item))
+            .map(item => item.map(value => `<td>${value}</td>`))
+            .map(tds => `<tr>${joinLists(tds)}</tr>`)
+
+        const template = `
+        <table class="table">
+        <thead>
+          ${joinLists(tHeaders)}
+        </thead>
+        <tbody>
+          ${joinLists(tBody)}
+        </tbody>
+      </table>
+        `
+        return template
+    }
+}
